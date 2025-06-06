37a40dfab98b2a82caae0a8f5054095f357410ae
proyecto23456 <fredyantoniocardona2@gmail.com>
2025-06-06 10:31:29 -0600

    Update miproyect.js

--- miproyect.js
+++ miproyect.js
@@ -3,14 +3,14 @@ function entrar() {
     const NOMBRE = document.getElementById("NOMBRE").value;
     const clave = document.getElementById("clave").value;
     const usuarios = {
-        "fredy": "123",
+        "sintia": "123",
         "oscar": "125",
         "Emiliana":"222",
         "Bravo": "456",
         "Ruben":"111",
         "Bravo": "456",
         "Astrid": "123"
-    "sintia": "123",
+
     };
 
     if (usuarios[NOMBRE] === clave) {
